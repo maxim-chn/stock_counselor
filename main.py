ab2b7847c642aa53f9f844f1f3dab7d164932327
MAXIM CHANTURIAY <maxim.chanturiay@ibm.com>
2021-03-12 15:09:44 +0200

    refactored the data_gathering_main_service invocation.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from data_gathering_main_service.boundary import startDataGatheringMainService
+from sys import argv
 
 if __name__ == '__main__':
-  startDataGatheringMainService()
+  if argv[1] == "data_gathering_main_service":
+    startDataGatheringMainService()
