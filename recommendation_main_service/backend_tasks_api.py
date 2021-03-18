9429e940b4033346214d79072cfa0d513ed3c1c9
MAXIM CHANTURIAY <maxim.chanturiay@ibm.com>
2021-03-18 18:26:47 +0200

    refactored the recommendation-main-service backend_tasks manager

--- recommendation_main_service/backend_tasks_api.py
+++ recommendation_main_service/backend_tasks_api.py
@@ -1,52 +1,57 @@
-"""
-Communicates with an in-memory database that manages the backend tasks.
-"""
-
-import json
+from json import dump, load
 from os import path
 
-from common_classes.loggable import Loggable
+from common.loggable_api import Loggable
+from common.backend_task_progress import BackendTaskProgress
 
-class BackendTasksApi(Loggable):
+class BackendTasks(Loggable):
   """
-  database_connection - dictionary
+  CRUDs the backend_tasks directly through the file system.
+  TODO: implement direct communication with an in-memory database, i.e. Redis, and remove any file system use in v1.0
   """
-  def __init__(self, database_connection=dict()):
-    # TODO: connect to module that communicates with RAM
-    super().__init__("BackendTasksApi")
-    self._database_connection = database_connection
+  def __init__(self, log_id):
+    """
+    Keyword arguments:
+      log_id -- str.
+    """
+    super().__init__(log_id, "BackendTasks")
 
-
-  def createTaskByUserId(self, user_id, progress):
-    self._debug(
-      "createTaskByUserId",
-      "Start - user_id: %s, progress: %s" % (user_id, progress)
-    )
-    task = {"user_id": user_id, "progress": progress}
-    relative_location = self._getRelativeLocation(user_id)
-    with open(relative_location, "w") as write_file:
-        json.dump(task, write_file)
-    result = "task has been created"
-
-    self._debug("createTaskByUserId", "Finish - result: %s" % result)
-    return result
+  def createTaskByUserId(self, user_id):
+    """
+    Keyword arguments:
+      user_id -- str -- an unique identifier of a user inside our program.
+    """
+    self._debug("createTaskByUserId", "Start - user_id: %s" % user_id)
+    task = {
+      "user_id": user_id,
+      "progress": BackendTaskProgress.STARTED.value
+    }
+    backend_task_path = self._getBackendTaskPath(user_id)
+    with open(backend_task_path, "w+") as write_file:
+      dump(task, write_file)
+    self._debug("createTaskByUserId", "Finish")
 
   def getTaskByUserId(self, user_id):
+    """
+    Returns a dict with the information about a backend task.
+    Keyword arguments:
+      user_id -- str -- an unique identifier of a user inside our program.
+    """
     self._debug("getTaskByUserId", "Start - user_id: %s" % user_id)
     result = None
-
-    relative_location = self._getRelativeLocation(user_id)
-    if path.exists(relative_location):
-      with open(relative_location, "r") as read_file:
-        result = json.load(read_file)
-
-    self._debug("getTaskByUserId", "Start - result: %s" % result)
+    backend_task_path = self._getBackendTaskPath(user_id)
+    if path.exists(backend_task_path):
+      with open(backend_task_path, "r") as read_file:
+        result = load(read_file)
+    self._debug("getTaskByUserId", "Finish - result: %s" % result)
     return result
 
-  def _getRelativeLocation(self, user_id):
+  def _getBackendTaskPath(self, user_id):
     return path.join(
       path.dirname(__file__),
-      "backend_tasks",
-      "remove_in_alpha",
-      '%s.json' % user_id
+      "..",
+      "common",
+      "backend_tasks_db",
+      "recommendation_tasks_db",
+      "%s.json" % user_id
     )
