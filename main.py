265bfe430b81c8272d9c59c587df59fb989920e9
gavriel arbov <gabro134@gmail.com>
2021-04-23 13:02:09 +0300

    mongodb initialize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from data_gathering_main_service.boundary_api import startDataGatheringMainService
 from data_gathering_worker_service.worker_api import Worker as DataGatheringWorker
+from init_scripts.mongodb import initializeMongoDb
 from recommendation_main_service.boundary import startRecommendationMainService
 from recommendation_worker_service.worker_api import Worker as RecommendationWorker
 from sys import argv
@@ -13,3 +14,5 @@ if __name__ == '__main__':
     startRecommendationMainService()
   elif argv[1] == "recommendation_worker_service":
     RecommendationWorker().startRecommendationWorkerService()
+  elif argv[1] == "init_mongodb":
+    initializeMongoDb()
