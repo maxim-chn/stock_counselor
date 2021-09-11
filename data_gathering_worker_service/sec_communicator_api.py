b9f2357693276368d0e5066bbf61e3b2d483eeec
Maxim Chanturiay <ardorm79@hotmail.co.il>
2021-09-11 16:10:37 +0300

    data gathering worker service is gathering and storing data with RabbitMq and MongoDb communication.

--- data_gathering_worker_service/sec_communicator_api.py
+++ data_gathering_worker_service/sec_communicator_api.py
@@ -5,6 +5,7 @@ from data_gathering_worker_service.sec_communicator.parser_for_financial_stateme
 from json import dumps, load
 from os import path
 from requests import post, get
+from traceback import format_exc
 
 class SecCommunicator(Loggable):
   """
@@ -12,8 +13,13 @@ class SecCommunicator(Loggable):
   official website at https://www.sec.gov/
   """
 
-  def __init__(self, log_id):
-    super().__init__(log_id, "SecCommunicator")
+  def __init__(self, service_name):
+    """
+    Keyword arguments:
+      service_name -- str.
+    """
+    super().__init__(service_name, "SecCommunicator")
+    self._max_error_chars = 5000
     self._reqs_to_sec_config = self._loadHttpRequestsToSecConfigurations()
     self._parser_for_10k_search_results = ParserFor10kSearchResults()
     self._financial_statements_ids_parser = FinancialStatementsIdsParser()
@@ -21,176 +27,241 @@ class SecCommunicator(Loggable):
 
   def get10kAccessionNumbers(self, available_financial_reports):
     """
-    Returns a dict:
+    Returns dict.
      - key -- str -- represents 10-K financial report date.
-     - value -- str -- an unique 10-K financial report id at the https://www.sec.gov/
+     - value -- str -- unique 10-K financial report id at the EDGAR database.
+    Raises RuntimeError.
     Keyword arguments:
       available_financial_reports -- str -- HTML Document with the financial statements available for a company.
     """
-    self._debug("get10kAccessionNumbers", "Start")
-    self._parser_for_10k_search_results.initFlagsAndResult()
-    self._parser_for_10k_search_results.feed(available_financial_reports)
-    result = self._parser_for_10k_search_results.getResult()
-    self._debug("get10kAccessionNumbers", "Finish - result: %s" % result)
-    return result
+    try:
+      self._debug("get10kAccessionNumbers", "Start")
+      
+      self._parser_for_10k_search_results.initFlagsAndResult()
+      self._parser_for_10k_search_results.feed(available_financial_reports)
+      result = self._parser_for_10k_search_results.getResult()
+      
+      self._debug("get10kAccessionNumbers", "Finish\nresult:\t%s" % result)
+      return result
+    except Exception as err:
+      err_msg = "%s -- get10kAccessionNumbers -- Failed\n%s" % (
+        self._class_name,
+        format_exc(self._max_error_chars, err)
+      )
+      raise RuntimeError(err_msg)
 
   def get10KReport(self, company_id, accession_number):
     """
-    Returns a str or None.
-    The str is a HTML Document with the 10-K financial report.
+    Returns str or None.
+    Raises RuntimeError.
+    Retrieves a HTML Document with the 10-K financial report.
     The report is expected to include the available financial statements types, i.e. Balance Sheets.
     Keyword arguments:
-      accession_number -- str -- unique 10-K report id at the https://www.sec.gov/
-      company_id -- str -- unique company id at the https://www.sec.gov/
+      accession_number -- str -- unique 10-K report id at the EDGAR database.
+      company_id -- str -- unique company id at the EDGAR database.
     """
-    self._debug(
-      "get10KReport", "Start - company_id: %s, accession_number: %s" % (company_id, accession_number)
-    )
-    result = None
-    config = self._reqs_to_sec_config["obtain_income_statement_ids"]
-    dst_url = config["url"]
-    req_headers = config["headers"]
-    req_params = config["params"]
-    req_params["accession_number"] = accession_number
-    req_params["cik"] = company_id
     try:
+      self._debug("get10KReport", "Start\ncompany_id:\t%s\naccession_number:\t%s" % (company_id, accession_number))
+      
+      result = None
+      config = self._reqs_to_sec_config["obtain_income_statement_ids"]
+      dst_url = config["url"]
+      req_headers = config["headers"]
+      req_params = config["params"]
+      req_params["accession_number"] = accession_number
+      req_params["cik"] = company_id
+      
       sec_response = get(dst_url, headers=req_headers, params=req_params)
       self._isSecReponseStatusCodeValid(sec_response, 200)
       self._isSecResponseContentTypeValid(sec_response, req_headers["Accept"])
       result = sec_response.text
-    except RuntimeError as err:
-      self._error("get10KReport", "Error - %s" % err)
-    finally:
+      
       if result:
-        self._debug("get10KReport", "Finish - result has %d characters" % len(result))
+        self._debug("get10KReport", "Finish\nresult has %d characters" % len(result))
       else:
-        self._debug("get10KReport", "Finish - result: None")
+        self._debug("get10KReport", "Finish\nresult: None")
       return result
+    except RuntimeError as err:
+      err_msg = "%s -- get10KReport -- Failed\n%s" % (self._class_name, str(err))
+      raise RuntimeError(err_msg)
+    except Exception as err:
+      err_msg = "%s -- get10KReport -- Failed\n%s" % (
+        self._class_name,
+        format_exc(self._max_error_chars, err)
+      )
+      raise RuntimeError(err_msg)
 
   def get10kReportsSearchResults(self, company_id):
     """
-    Returns a str or None.
-    The str is a HTML Document with the search results for the 10-K financial reports.
+    Returns str or None.
+    Raises RuntimeError.
+    Retrieves HTML Document with the search results for the 10-K financial reports.
     Keyword arguments:
-      company_id -- str -- unique company id at the https://www.sec.gov/
+      company_id -- str -- unique company id at the EDGAR database.
     """
-    self._debug("get10kReportsSearchResults", "Start - company_id: %s" % company_id)
-    result = None
-    config = self._reqs_to_sec_config["obtain_10k_filings"]
-    dst_url = config["url"]
-    req_headers = config["headers"]
-    req_params = config["params"]
-    req_params["CIK"] = company_id
     try:
+      self._debug("get10kReportsSearchResults", "Start\ncompany_id:\t%s" % company_id)
+      result = None
+      config = self._reqs_to_sec_config["obtain_10k_filings"]
+      dst_url = config["url"]
+      req_headers = config["headers"]
+      req_params = config["params"]
+      req_params["CIK"] = company_id
+      
       sec_response = get(dst_url, headers=req_headers, params=req_params)
       self._isSecReponseStatusCodeValid(sec_response, 200)
       self._isSecResponseContentTypeValid(sec_response, req_headers["Accept"])
       result = sec_response.text
-    except RuntimeError as err:
-      self._error("get10kReportsSearchResults", "Error - %s" % err)
-    finally:
+      
       if result:
-        self._debug("get10kReportsSearchResults", "Finish - result has %d characters" % len(result))
+        self._debug("get10kReportsSearchResults", "Finish\nresult has %d characters" % len(result))
       else:
-        self._debug("get10kReportsSearchResults", "Finish - result: None")
+        self._debug("get10kReportsSearchResults", "Finish\nresult: None")
       return result
+    except RuntimeError as err:
+      err_msg = "%s -- get10kReportsSearchResults -- Failed\n%s" % (self._class_name, str(err))
+      raise RuntimeError(err_msg)
+    except Exception as err:
+      err_msg = "%s -- get10kReportsSearchResults -- Failed\n%s" % (
+        self._class_name,
+        format_exc(self._max_error_chars, err)
+      )
+      raise RuntimeError(err_msg)
 
-  def getCompanyId(self, acronym):
+  def getCompanyId(self, company_acronym):
     """
-    Returns a str or None.
-    The str is the unique id of a company, i.e. Microsoft, at the https://www.sec.gov/.
+    Returns str or None.
+    Retrieves the unique id of a company, i.e. Microsoft, at the EDGAR database.
     Keyword arguments:
-      acronym -- str -- the company symbol at the stock exchange, i.e. NASDAQ.
+      company_acronym -- str -- the company symbol at a stock exchange, i.e. NASDAQ.
     """
-    self._debug("getCompanyId", "Start - acronym: %s" % acronym)
-    result = None
-    config = self._reqs_to_sec_config["obtain_company_id"]
-    dst_url = config["url"]
-    req_headers = config["headers"]
-    req_payload = config["payload"]
-    req_payload["keysTyped"] = acronym
     try:
+      self._debug("getCompanyId", "Start\nacronym:\t%s" % company_acronym)
+      
+      result = None
+      config = self._reqs_to_sec_config["obtain_company_id"]
+      dst_url = config["url"]
+      req_headers = config["headers"]
+      req_payload = config["payload"]
+      req_payload["keysTyped"] = company_acronym
+      
       sec_response = post(dst_url, data=dumps(req_payload), headers=req_headers)
       self._isSecReponseStatusCodeValid(sec_response, 200)
       self._isSecResponseContentTypeValid(sec_response, req_headers["Accept"])
       response_data = sec_response.json()
-      self._isCompanyAcronymInSecResponseValid(response_data, acronym)
+      self._isCompanyAcronymInSecResponseValid(response_data, company_acronym)
       result = response_data["hits"]["hits"][0]["_id"]
-    except (IndexError, RuntimeError) as err:
-      self._error("getCompanyId", "Error - %s" % err)
-    finally:
-      self._debug("getCompanyId", "Finish - result: %s" % result)
+      self._debug("getCompanyId", "Finish\nresult:\t%s" % result)
       return result
+    except RuntimeError as err:
+      err_msg = "%s -- getCompanyId -- Failed\n%s" % (self._class_name, str(err))
+      raise RuntimeError(err_msg)
+    except Exception as err:
+      err_msg = "%s -- getCompanyId -- Failed\n%s" % (self._class_name, format_exc(self._max_error_chars, err))
+      raise RuntimeError(err_msg)
 
   def getDataFromFinancialStatement(self, financial_statement):
     """
-    Returns a tuple:
+    Returns a tuple.
       - index 0 -- dict -- includes the retrieved financial data from https://www.sec.gov/.
       - index 1 -- str -- currency units.
+    Raises RuntimeError.
     Keyword arguments:
       financial_statement -- str -- HTML Document with the financial data.
     """
-    self._debug("getDataFromFinancialStatement", "Start")
-    self._financial_statement_parser.initFlagsAndResult()
-    self._financial_statement_parser.feed(financial_statement)
-    result = self._financial_statement_parser.getResult()
-    self._debug(
-      "getDataFromFinancialStatement",
-      "Finish - result: %s financial values, %s currency format" % (len(result[0].keys()), result[1])
-    )
-    return result
+    try:
+      self._debug("getDataFromFinancialStatement", "Start")
+      
+      self._financial_statement_parser.initFlagsAndResult()
+      self._financial_statement_parser.feed(financial_statement)
+      result = self._financial_statement_parser.getResult()
+      
+      self._debug(
+        "getDataFromFinancialStatement",
+        "Finish - result: %s financial values, %s currency units" % (len(result[0].keys()), result[1])
+      )
+      return result
+    except Exception as err:
+      err_msg = "%s -- getDataFromFinancialStatement -- Failed\n%s" % (
+        self._class_name,
+        format_exc(self._max_error_chars, err)
+      )
+      raise RuntimeError(err_msg)
 
   def getFinancialStatement(self, company_id, accession_number, financial_statement_type):
     """
-    Returns a str or None.
+    Returns str or None.
+    Raises RuntimeError.
     The str is a HTML Document with the financial data statement, i.e Balance Sheets.
     Keyword arguments:
-       accession_number -- str -- unique 10-K report id at the https://www.sec.gov/
-       company_id -- str -- unique company id at the https://www.sec.gov/
-       financial_statement_type -- str -- financial statement type id, i.e. R2.
+      - accession_number -- str -- unique 10-K report id at the https://www.sec.gov/
+      - company_id -- str -- unique company id at the https://www.sec.gov/
+      - financial_statement_type -- str -- financial statement type id, i.e. R2.
     """
-    self._debug(
-      "getFinancialStatement",
-      "Start - company_id: %s, accession_number: %s, income_statement_id: %s" % (
-        company_id, accession_number, financial_statement_type
-      )
-    )
-    result = None
-    config = self._reqs_to_sec_config["obtain_income_statement_data"]
-    dst_url = "%s/%s/%s/%s.htm" % (config["url"], company_id, accession_number, financial_statement_type)
-    req_headers = config["headers"]
     try:
+      self._debug(
+        "getFinancialStatement",
+        "Start - company_id: %s, accession_number: %s, income_statement_id: %s" % (
+          company_id,
+          accession_number,
+          financial_statement_type
+        )
+      )
+      
+      result = None
+      config = self._reqs_to_sec_config["obtain_income_statement_data"]
+      dst_url = "%s/%s/%s/%s.htm" % (config["url"], company_id, accession_number, financial_statement_type)
+      req_headers = config["headers"]
+      
       sec_response = get(dst_url, headers=req_headers)
       self._isSecReponseStatusCodeValid(sec_response, 200)
       self._isSecResponseContentTypeValid(sec_response, req_headers["Accept"])
       result = sec_response.text
-    except RuntimeError as err:
-      self._error("getFinancialStatement", "Error - %s" % err)
-    finally:
+      
       if result:
-        self._debug("getFinancialStatement", "Finish result has %d characters" % len(result))
+        self._debug("getFinancialStatement", "Finish\nresult has %d characters" % len(result))
       else:
-        self._debug("getFinancialStatement", "Finish - result: None")
+        self._debug("getFinancialStatement", "Finish\nresult: None")
       return result
+    except RuntimeError as err:
+      err_msg = "%s -- getFinancialStatement -- Failed\n%s" % (self._class_name, str(err))
+      raise RuntimeError(err_msg)
+    except Exception as err:
+      err_msg = "%s -- getFinancialStatement -- Failed\n%s" % (
+        self._class_name,
+        format_exc(self._max_error_chars, err)
+      )
+      raise RuntimeError(err_msg)
 
   def getFinancialStatementsTypes(self, report_10k):
     """
-    Returns a list of strings.
-    Each str represents the available type of financial statements.
+    Returns list.
+    Raises RuntimeError.
+    Each element is a str. It represents the available type of financial statements.
     Keyword arguments:
       report_10k -- str -- a HTML Document with the 10-K financial report.
     """
-    self._debug("getFinancialStatementsTypes", "Start")
-    self._financial_statements_ids_parser.initFlagsAndResult()
-    self._financial_statements_ids_parser.feed(report_10k)
-    result = self._financial_statements_ids_parser.getResults()
-    self._debug("getFinancialStatementsTypes", "Finish - result: %s" % result)
-    return result
+    try:
+      self._debug("getFinancialStatementsTypes", "Start")
+      
+      self._financial_statements_ids_parser.initFlagsAndResult()
+      self._financial_statements_ids_parser.feed(report_10k)
+      result = self._financial_statements_ids_parser.getResults()
+      
+      self._debug("getFinancialStatementsTypes", "Finish\nresult:\t%s" % result)
+      return result
+    except Exception as err:
+      err_msg = "%s -- getFinancialStatementsTypes -- Failed\n%s" % (
+        self._class_name,
+        format_exc(self._max_error_chars, err)
+      )
+      raise RuntimeError(err_msg)
+    
 
   def _getHttpRequestsToSecConfigFile(self):
     """
-    TODO: remove in v1.0.
-    Returns a path to the configuration file for the HTTP requests to the https://www.sec.gov/
+    Returns path.
+    It leads to the configuration file for the HTTP requests to the https://www.sec.gov/
     """
     return path.join(
       path.dirname( __file__ ),
@@ -238,7 +309,8 @@ class SecCommunicator(Loggable):
 
   def _loadHttpRequestsToSecConfigurations(self):
     """
-    Returns a dict. It is the configurations for the HTTP Requests to the http://www.sec.gov/.
+    Returns a dict.
+    It is the configurations for the HTTP Requests to the http://www.sec.gov/.
     """
     with open(self._getHttpRequestsToSecConfigFile(), "r") as read_file:
         result = load(read_file)
--- data_gathering_worker_service/worker_api.py
+++ data_gathering_worker_service/worker_api.py
@@ -6,6 +6,7 @@ from common.loggable_api import Loggable
 from datetime import datetime
 from data_gathering_worker_service.financial_report import FinancialReport
 from data_gathering_worker_service.sec_communicator_api import SecCommunicator
+from time import sleep
 from traceback import format_exc
 
 def startDataGatheringWorkerService(service_name):
@@ -21,11 +22,12 @@ def startDataGatheringWorkerService(service_name):
     Returns void.
     Logic for the execution of the financial data gathering task.
     Keyword arguments:
-      ch -- Channel -- RabbitMq channel.
-      method -- ??? -- ???
-      properties -- ??? -- ???
-      body -- ??? -- contains the message.
+      - ch -- Channel -- RabbitMq channel.
+      - method -- ??? -- ???
+      - properties -- ??? -- ???
+      - body -- ??? -- contains the message.
     """
+    service_name = "data_gathering_worker_service"
     worker = Worker(service_name)
     max_error_chars = 5000
     task = None
@@ -38,7 +40,7 @@ def startDataGatheringWorkerService(service_name):
         service_name,
         format_exc(max_error_chars, err)
       )
-      getLogger().error(err_msg)
+      getLogger(service_name).error(err_msg)
       return
     finally:
       ch.stop_consuming()
@@ -50,11 +52,12 @@ def startDataGatheringWorkerService(service_name):
         service_name,
         str(err)
       )
+      getLogger(service_name).error(err_msg)
   
   try:
     backend_tasks = BackendTasks(service_name)
   except RuntimeError as err:
-    err_msg = "startDataGatheringWorkerService -- Failed to initiate backend tasks object\n%s" % (
+    err_msg = "%s -- startDataGatheringWorkerService -- Failed to initiate backend tasks object\n%s" % (
       service_name,
       str(err)
     )
@@ -63,7 +66,7 @@ def startDataGatheringWorkerService(service_name):
   try:
     backend_tasks.consumeMessage(consumeFinancialDataGatheringMessages)
   except RuntimeError as err:
-    err_msg = "startDataGatheringWorkerService -- Failed to subscribe to backend tasks\n%s" % (
+    err_msg = "%s -- startDataGatheringWorkerService -- Failed to subscribe to backend tasks\n%s" % (
       service_name,
       str(err)
     )
@@ -83,6 +86,7 @@ class Worker(Loggable):
     super().__init__(service_name, "Worker")
     self._backend_tasks = BackendTasks(service_name)
     self._database = DatabaseApi(service_name)
+    self._date_format = '%Y-%m-%d'
     self._farthest_date_relevant = datetime.strptime("2015-01-01", '%Y-%m-%d')
     self._sec_communicator = SecCommunicator(service_name)
 
@@ -92,98 +96,143 @@ class Worker(Loggable):
     Raises RuntimeError.
     Collects financial data and stores it in the database.
     Keyword arguments:
-      task -- Task -- represents a backend task for financial data gathering.
+      - task -- Task -- represents a backend task for financial data gathering.
     """
-    self._debug("getFinancialData", "Start\ntask:\t%s" % str(task))
-    financial_reports = []
-    task.progress = Progress.QUERYING_FOR_COMPANY_ID
-    self._backend_tasks.updateTaskProgressBy(task)
-    company_id = self._sec_communicator.getCompanyId(task.company_acronym)
-
-    if company_id:
+    try:
+      self._debug("getFinancialData", "Start\ntask:\t%s" % str(task))
+      financial_reports = []
+      
+      task.progress = Progress.QUERYING_FOR_COMPANY_ID
+      self._backend_tasks.updateTaskProgressBy(task)
+      company_id = self._sec_communicator.getCompanyId(task.company_acronym)
+      
+      if not company_id:
+        self._debug("getFinancialData", "Finish\nExpected to get Company Id, but got None")
+        return
+      
       task.progress = Progress.QUERYING_FOR_AVAILABLE_10K_REPORTS
       self._backend_tasks.updateTaskProgressBy(task)
+      sleep(1)
       html_with_10k_search_results = self._sec_communicator.get10kReportsSearchResults(company_id)
 
-      if html_with_10k_search_results:
-        task.progress = Progress.QUERYING_FOR_10K_IDS
+      if not html_with_10k_search_results:
+        self._debug("getFinancialData", "Finish\nExpected to get Html with 10K Search Results, but got None")
+        return
+
+      task.progress = Progress.QUERYING_FOR_10K_IDS
+      self._backend_tasks.updateTaskProgressBy(task)
+      sleep(1)
+      accession_numbers = self._sec_communicator.get10kAccessionNumbers(html_with_10k_search_results)
+
+      if not accession_numbers:
+        self._debug(
+          "getFinancialData",
+          "Finish\nExpected to get mapping of years to accession numbers, but got empty dict"
+        )
+        return
+      
+      for date_str, accession_number in accession_numbers.items():
+        next_date = datetime.strptime(date_str, self._date_format)
+        
+        if next_date < self._farthest_date_relevant:
+          continue
+        
+        financial_report = FinancialReport()
+        financial_report.date = next_date
+        
+        task.progress = Progress.QUERYING_FOR_10K_REPORT_CONTENTS
         self._backend_tasks.updateTaskProgressBy(task)
-        accession_numbers = self._sec_communicator.get10kAccessionNumbers(html_with_10k_search_results)
+        sleep(1)
+        html_with_10k_report = self._sec_communicator.get10KReport(company_id, accession_number)
 
-        if accession_numbers:
-          for date_str, accession_number in accession_numbers.items():
-            next_date = datetime.strptime(date_str, '%Y-%m-%d')
-            
-            if next_date < self._farthest_date_relevant:
-              continue
-            
-            financial_report = FinancialReport()
-            task.progress = Progress.QUERYING_FOR_10K_REPORT_CONTENTS
-            self._backend_tasks.updateTaskProgressBy(task)
-            html_with_10k_report = self._sec_communicator.get10KReport(company_id, accession_number)
+        if not html_with_10k_report:
+          self._debug("getFinancialData", "\tExpected to get Html with 10K Report for %s, but got None" % date_str)
+          continue
 
-            if not html_with_10k_report:
-              continue
+        task.progress = Progress.QUERYING_FOR_FINANCIAL_STMNT_TYPES
+        self._backend_tasks.updateTaskProgressBy(task)
+        sleep(1)
+        financial_statement_types = self._sec_communicator.getFinancialStatementsTypes(html_with_10k_report)
 
-            task.progress = Progress.QUERYING_FOR_FINANCIAL_STMNT_TYPES
-            self._backend_tasks.updateTaskProgressBy(task)
-            financial_statement_types = self._sec_communicator.getFinancialStatementsTypes(html_with_10k_report)
+        if not financial_statement_types:
+          self._debug("getFinancialData", "\tExpected to get Financial Statement Types for %s, but got None" % date_str)
+          return
 
-            for financial_statement_type in financial_statement_types:
-              task.progress = Progress.QUERYING_FOR_FINANCIAL_STMNT_CONTENTS
-              self._backend_tasks.updateTaskProgressBy(task)
-              html_with_financial_statement = self._sec_communicator.getFinancialStatement(
-                company_id,
-                accession_number,
-                financial_statement_type
+        for financial_statement_type in financial_statement_types:
+          task.progress = Progress.QUERYING_FOR_FINANCIAL_STMNT_CONTENTS
+          self._backend_tasks.updateTaskProgressBy(task)
+          html_with_financial_statement = self._sec_communicator.getFinancialStatement(
+            company_id,
+            accession_number,
+            financial_statement_type
+          )
+
+          if not html_with_financial_statement:
+            self._debug(
+              "getFinancialData",
+              "\tExpected to get Financial Statement for type %s, %s, but got None" % (
+                financial_statement_type,
+                date_str
               )
+            )
+            continue
 
-              if not html_with_financial_statement:
-                continue
-
-              financial_data = self._sec_communicator.getDataFromFinancialStatement(html_with_financial_statement)
-              
-              if not financial_report.currencyUnitsUpdated and financial_data[1]:
-                financial_report.currency_units = financial_data[1]
-              
-              existing_financial_measurements = financial_report.measurements
-              financial_report.measurements = existing_financial_measurements | financial_data[0]
-            financial_reports.append(financial_report)
-    
-    self._persistFinancialReports(financial_reports)
-    self._debug("getFinancialData", "Finish")
+          financial_data = self._sec_communicator.getDataFromFinancialStatement(html_with_financial_statement)
+          
+          if not financial_data[0]:
+            self._debug(
+              "getFinancialData",
+              "\tExpected to get Financial Data for %s, %s, but got None" % (financial_statement_type, date_str)
+            )
+            continue
+          
+          if not financial_report.currencyUnitsUpdated() and financial_data[1]:
+            financial_report.currency_units = financial_data[1]
+          
+          existing_financial_measurements = financial_report.measurements
+          financial_report.measurements = existing_financial_measurements | financial_data[0]
+        
+        financial_reports.append(financial_report)
+      
+      self._persistFinancialReports(financial_reports)
+      
+      task.progress = Progress.FINISHED
+      self._backend_tasks.updateTaskProgressBy(task)
+      
+      self._debug("getFinancialData", "Finish")
+    except RuntimeError as err:
+      err_msg = "%s -- Failed\n%s" % (self._class_name, str(err))
+      raise RuntimeError(err_msg)
 
   def _persistFinancialReport(self, financial_report):
     """
     Returns void.
     Raises RuntimeError.
     Keyword arguments:
-      financial_report -- FinancialReport.
+      - financial_report -- FinancialReport.
     """
-    self._debug("_persistFinancialReport", "Start\nfinancial_report:\t%s" % str(financial_report))
-
     try:
+      self._debug("_persistFinancialReport", "Start\nfinancial_report:\t%s" % str(financial_report))
       self._database.createFinancialReportDocument(financial_report.toDocument())
+      self._debug("_persistFinancialReport", "Finish")
     except RuntimeError as err:
       err_msg = "%s -- _persistFinancialReport -- Failed.\n%s" % (self._class_name, str(err))
       raise RuntimeError(err_msg)
 
-    self._debug("_persistFinancialReport", "Finish")
 
   def _persistFinancialReports(self, financial_reports):
     """
     Returns void.
     Raises RuntimeError.
     Keyword arguments:
-      financial_reports -- list -- contains FinancialReport objects.
+      - financial_reports -- list -- contains FinancialReport objects.
     """
-    self._debug("_persistFinancialReports", "Start\nAmount of years with financial report" % len(financial_reports))
-
     try:
+      self._debug("_persistFinancialReports", "Start\nAmount of years with financial report: %d" % len(financial_reports))
       for financial_report in financial_reports:
         self._persistFinancialReport(financial_report)
+      self._debug("_persistFinancialReports", "Finish")
     except RuntimeError as err:
       err_msg = "%s -- _persistFinancialReports -- Failed.\n%s" % (self._class_name, str(err))
       raise RuntimeError(err_msg)
     
-    self._debug("_persistFinancialReports", "Finish")
